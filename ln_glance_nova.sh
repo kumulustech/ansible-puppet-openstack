94b5e7ef00a89ba801d20b29f0d197fb894e8ef7
Robert Starmer <rstarmer@gmail.com>
2015-07-07 23:01:37 -0700

    update gluster glance to exclude nova

--- ln_glance_nova.sh
+++ ln_glance_nova.sh
@@ -3,8 +3,8 @@
 cd /var/lib
 mv glance glance.orig
 ln -s /mnt/storage-pool/glance glance
-mv nova nova.orig
-ln -s /mnt/storage-pool/nova nova
-rm -rf nova.orig glance.orig
+#mv nova nova.orig
+#ln -s /mnt/storage-pool/nova nova
+#rm -rf nova.orig glance.orig
 
 rm /mnt/storage-pool/migrated
--- mv_glance_nova.sh
+++ mv_glance_nova.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 cd /var/lib
-tar cfz - nova/ | (cd /mnt/storage-pool/; tar xfz -)
-mv nova nova.orig
-ln -s /mnt/storage-pool/nova
-chown -R nova.nova nova
-rm -rf nova.orig
+#tar cfz - nova/ | (cd /mnt/storage-pool/; tar xfz -)
+#mv nova nova.orig
+#ln -s /mnt/storage-pool/nova
+#chown -R nova.nova /mnt/storage-pool/nova
+#chown -R nova.nova /var/lib/nova
+#rm -rf nova.orig
 tar cfz - glance/ | (cd /mnt/storage-pool/; tar xfz -)
 mv glance glance.orig
 ln -s /mnt/storage-pool/glance
-chown -R glance.glance glance
+chown -R glance.glance /mnt/storage-pool/glance
+chown -R glance.glance /var/lib/glance
 rm -rf glance.orig
 
 touch /mnt/storage-pool/migrated
